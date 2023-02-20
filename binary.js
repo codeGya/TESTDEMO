4fa2a37dc8e99d2b12fd6596c0c7fc218b2bb51e
codeGya <singhindresh400@gmail.com>
2023-02-20 11:32:08 +0530

    hey

---
+++ binary.js
@@ -0,0 +1,12 @@
+let array=[1,0,-9,4,5,3,2,1]
+for(let i=1;i<array.length;i=i+1){
+    for(let j=i-1;j>=0;j=j-1){
+        if(array[j+1]<array[j]){
+            let temp=array[j]
+            array[j]=array[j+1]
+            array[j+1]=temp
+
+        }
+    }
+}
+console.log(array)
